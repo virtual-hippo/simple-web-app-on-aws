5f7aa1781f183423ba5f77e1e60e4e377f2fea30
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2024-05-05 18:32:01 +0900

    [#18] update: Updated CDK entry point to create pipeline stack

--- apps/cdk/bin/simple-web-app-on-aws.ts
+++ apps/cdk/bin/simple-web-app-on-aws.ts
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
-import { SimpleWebAppStack } from "../lib/stacks/simple-web-app-stack";
+import { SimpleWebAppPipelineStack } from "../lib/stacks/simple-web-app-pipeline-stack";
 
 const app = new cdk.App();
-const stack = new SimpleWebAppStack(app, "SimpleWebAppStack", {});
+const stack = new SimpleWebAppPipelineStack(
+  app,
+  "SimpleWebAppPipelineStack",
+  {}
+);
 
 cdk.Tags.of(stack).add("SysName", "simple-web-app-on-aws");
