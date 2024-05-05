e6e9a3834f12920f6a35d061f2d4c963443394f2
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2024-05-05 18:50:10 +0900

    [#18] update: Updated SimpleWebAppStage to set tag by using cdk.Tags.of()

--- apps/cdk/lib/stages/simple-web-app-stage.ts
+++ apps/cdk/lib/stages/simple-web-app-stage.ts
@@ -1,23 +1,22 @@
-import { Environment, Stage } from "aws-cdk-lib";
+import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { SimpleWebAppStack } from "../stacks/simple-web-app-stack";
 
 interface AppParameter {
-  env?: Environment;
+  env?: cdk.Environment;
 }
 
-export class SimpleWebAppStage extends Stage {
+export class SimpleWebAppStage extends cdk.Stage {
   constructor(scope: Construct, id: string, props: AppParameter) {
     super(scope, id, props);
 
-    const simpleWebApp = new SimpleWebAppStack(this, "SimpleWebApp", {
+    const simpleWebAppStack = new SimpleWebAppStack(this, "SimpleWebApp", {
       env: {
         account: props.env?.account || process.env.CDK_DEFAULT_ACCOUNT,
         region: props.env?.region || process.env.CDK_DEFAULT_REGION,
       },
-      tags: {
-        SysName: "simple-web-app-on-aws",
-      },
     });
+
+    cdk.Tags.of(simpleWebAppStack).add("SysName", "simple-web-app-on-aws");
   }
 }
