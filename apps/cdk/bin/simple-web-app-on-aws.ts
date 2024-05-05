457298243bf15c41940565f108fe927403e85abf
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2024-05-05 15:24:33 +0900

    [#16] update: Renamed stack

--- apps/cdk/bin/simple-web-app-on-aws.ts
+++ apps/cdk/bin/simple-web-app-on-aws.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
-import { CdkStack } from "../lib/stacks/cdk-stack";
+import { SimpleWebAppStack } from "../lib/stacks/simple-web-app-stack";
 
 const app = new cdk.App();
-const stack = new CdkStack(app, "CdkStack", {});
+const stack = new SimpleWebAppStack(app, "SimpleWebAppStack", {});
 
 cdk.Tags.of(stack).add("SysName", "simple-web-app-on-aws");
--- apps/cdk/lib/stacks/simple-web-app-stack.ts
+++ apps/cdk/lib/stacks/simple-web-app-stack.ts
@@ -2,7 +2,7 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as lambdaFunc from "../constructs/lambda-func";
 
-export class CdkStack extends cdk.Stack {
+export class SimpleWebAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
